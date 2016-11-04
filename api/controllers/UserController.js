e8c1eff391b697fd0890d2c070e1fe30920fa101
Mike DeVita <mike@relative.media>
2016-11-04 10:50:20 -0700

    add a soft delete for various controllers

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,4 +5,16 @@
  * @description :: Server-side logic for manage users
  */
 
-module.exports = {};
+module.exports = {
+  checkUsername: (req, res) => {
+    User.count({ username: req.body.username })
+    .then(res.ok)
+    .catch(res.negotiate)
+  },
+  delete: (req, res) => {
+    sails.log.debug(req.params.id)
+    User.update(req.params.id, { isDeleted: true })
+    .then(res.ok)
+    .catch(res.negotiate)
+  }
+};
