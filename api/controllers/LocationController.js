96504b5ba54e4aa1f4cc14375e4af5d81255dc15
Mike DeVita <mike@relative.media>
2016-11-07 17:51:50 -0700

    check location name

--- api/controllers/LocationController.js
+++ api/controllers/LocationController.js
@@ -5,4 +5,10 @@
  * @description :: Server-side logic for manage locations
  */
 
-module.exports = {};
+module.exports = {
+  checkLocationname: (req, res) => {
+    Location.count({ name: req.body.name })
+    .then(res.ok)
+    .catch(res.negotiate)
+  }
+};
