4a8bcbcde0e474999f411b6e7829b9d59513dea1
Mike DeVita <mike@relative.media>
2016-11-09 06:41:25 -0700

    add a redis connector for sockets and afterDisconnect() logic.

--- config/sockets.js
+++ config/sockets.js
@@ -1,11 +1,16 @@
 module.exports.sockets = {
-  adapter: 'sails-redis',
+  adapter: 'socket.io-redis',
   host: '127.0.0.1',
   port: 6379,
   db: 'sapinfrastructure-sockets',
   afterDisconnect: function(session, socket, cb) {
-    sails.log.debug(socket)
-    Agent.unsubscribe(socket)
-    return cb();
+    sails.log.debug('Agent disconnect socketId:', socket.id)
+    Agent.update({ socketId: socket.id }, { connected: false })
+     .then(function(agent) {
+       var _agent = agent[0]
+       sails.log.debug('Agent id:', _agent.id, 'socketId:', socket.id, 'disconnected', socket.id);
+       sails.sockets.leave(socket, ['agents', 'agent-' + _agent.id])
+       return cb();
+     });
   }
 };
